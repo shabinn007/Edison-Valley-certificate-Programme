0f8695fe59049de3265a25e742e7d6104a1c2287
Shabin <114988892+shabinn007@users.noreply.github.com>
2023-03-28 14:11:31 +0530

    new api

--- src/Store/Axiosmethod.jsx
+++ src/Store/Axiosmethod.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASICURL = "http://api.certificates.edisonvalley.com/api/v1";
+const BASICURL = "https://wild-erin-cowboy-boots.cyclic.app/";
+// const BASICURL = "http://api.certificates.edisonvalley.com/api/v1";
 
 export const axiosApi = axios.create({
   baseURL: BASICURL,
